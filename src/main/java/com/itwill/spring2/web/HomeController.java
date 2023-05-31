ee2cc74dc32eaede8bd7c366374ae424e5eb2fde
chklee1048 <52231890+chklee1048@users.noreply.github.com>
2023-05-31 19:27:55 +0900

    test commit

--- src/main/java/com/itwill/spring2/web/HomeController.java
+++ src/main/java/com/itwill/spring2/web/HomeController.java
@@ -11,7 +11,7 @@ public class HomeController {
     @GetMapping("/")
     public String home() {
         
-        log.info("homeControler");
+        log.info("homeControleraa");
         
         return "index";
     }
